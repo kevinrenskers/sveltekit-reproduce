39ea7c88e5bc555d017c1fdd871eafda2bec8811
Anyass3 <nyassabu@gmail.com>
2021-08-16 00:50:24 +0000

    fixed briefly flashing old content

--- src/routes/protected/__layout.svelte
+++ src/routes/protected/__layout.svelte
@@ -1,32 +1,34 @@
 <script context="module">
-  import { get } from "svelte/store";
-  import { content } from "$lib/store";
-  import Navbar from "$lib/Navbar.svelte";
+  import { get } from 'svelte/store';
+  import { content } from '$lib/store';
+  import Navbar from '$lib/Navbar.svelte';
+  import { browser } from '$app/env';
 
   export async function load({ fetch, session }) {
     if (!session.jwt) {
       return {
         status: 302,
-        redirect: "/"
+        redirect: '/',
       };
     }
+    if (browser) {
+      const storedContent = get(content);
 
-    const storedContent = get(content);
-
-    if (storedContent) {
-      return {
-        props: {
-          fetchedContent: storedContent
-        }
-      };
+      if (storedContent) {
+        return {
+          props: {
+            fetchedContent: storedContent,
+          },
+        };
+      }
     }
 
-    const res = await fetch("/protected.json");
+    const res = await fetch('/protected.json');
 
     return {
       props: {
-        fetchedContent: await res.text()
-      }
+        fetchedContent: await res.text(),
+      },
     };
   }
 </script>
